562e359f127a581af892871217c28ca55e269ac6
Swami <anilsanapathi9@gmail.com>
2023-07-06 08:18:30 +0530

    WIP: new article

--- src/app/feature/create-article/create-article.component.ts
+++ src/app/feature/create-article/create-article.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormControl, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     standalone: true,
@@ -7,30 +9,55 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
             <div class="container page">
                 <div class="row">
                     <div class="col-md-10 offset-md-1 col-xs-12">
-                        <form>
+                        <form [formGroup]="form" (ngSubmit)="form.valid && submit()">
                             <fieldset>
                                 <fieldset class="form-group">
                                     <input
                                         type="text"
                                         class="form-control form-control-lg"
                                         placeholder="Article Title"
+                                        formControlName="title"
                                     />
                                 </fieldset>
                                 <fieldset class="form-group">
-                                    <input type="text" class="form-control" placeholder="What's this article about?" />
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="What's this article about?"
+                                        formControlName="description"
+                                    />
                                 </fieldset>
                                 <fieldset class="form-group">
                                     <textarea
                                         class="form-control"
                                         rows="8"
                                         placeholder="Write your article (in markdown)"
+                                        formControlName="body"
                                     ></textarea>
                                 </fieldset>
                                 <fieldset class="form-group">
-                                    <input type="text" class="form-control" placeholder="Enter tags" />
-                                    <div class="tag-list"></div>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="Enter tags"
+                                        #tagInput
+                                        (keyup.enter)="addTag(tagInput)"
+                                    />
+                                    <div class="tag-list" *ngIf="form.get('tagList')?.value?.length">
+                                        <span
+                                            class="tag-pill tag-default"
+                                            *ngFor="let tag of form.get('tagList')?.value"
+                                        >
+                                            <i class="ion-close-round" (click)="removeTag(tag)"></i>
+                                            {{ ' ' + tag }}
+                                        </span>
+                                    </div>
                                 </fieldset>
-                                <button class="btn btn-lg pull-xs-right btn-primary" type="button">
+                                <button
+                                    class="btn btn-lg pull-xs-right btn-primary"
+                                    type="button"
+                                    [disabled]="form.invalid"
+                                >
                                     Publish Article
                                 </button>
                             </fieldset>
@@ -40,6 +67,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
             </div>
         </div>
     `,
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [ReactiveFormsModule, NgFor, NgIf]
 })
-export default class CreateArticleComponent {}
+export default class CreateArticleComponent {
+    form = inject(NonNullableFormBuilder).group({
+        title: ['', [Validators.required, Validators.minLength(3)]],
+        description: ['', [Validators.required, Validators.minLength(3)]],
+        body: ['', [Validators.required, Validators.minLength(3)]],
+        tagList: new FormControl<string[]>([], [Validators.required])
+    });
+
+    addTag(tag: HTMLInputElement) {
+        if (!tag?.value) return;
+
+        this.form.value.tagList?.push(tag.value);
+    }
+
+    removeTag(tag: string) {
+        this.form.value.tagList = this.form?.value?.tagList?.filter((t) => t !== tag);
+    }
+
+    submit() {
+        console.log(this.form.getRawValue());
+    }
+}
--- src/app/models/model.ts
+++ src/app/models/model.ts
@@ -1,3 +1,4 @@
 export { Article } from './Article';
 export { Author } from './Author';
+export { NewArticle } from './NewArticle';
 export { User } from './User';
